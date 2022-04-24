8bc6f0af48f099e7786b142a04dacb79db6cac71
zhandos256 <r.zhandos256@gmail.com>
2022-04-24 21:01:00 +0600

    some refactor

---
+++ astana1/astana1.bat
@@ -0,0 +1,10 @@
+echo Downloading Jre-8u151.exe
+curl https://faucet.aas.duke.edu/pub/pc/bigfix/patches/java/jre-8u151-windows-i586.exe --output jre-8u151-windows-i586.exe
+echo Installing Jre-8u151.exe
+start jre-8u151-windows-i586.exe /s
+echo Downloading ASTANA 1 file
+COPY exception.sites C:\%userprofile%\AppData\LocalLow\Sun\Java\Deployment\security\
+curl http://212.154.167.54:8080/kazawclient1/AWhqLO_OUT_19.jnlp --output AWhqLO_OUT_19.jnlp 
+echo Installing ASTANA 1
+start hqLO_OUT_19.jnlp /s
+echo DONE
