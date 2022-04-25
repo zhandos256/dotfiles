74713c51d44fb9ee854e6e07435e1acda329f92d
zhandos256 <r.zhandos256@gmail.com>
2022-04-25 09:01:57 +0600

    some refactor

--- astana1/astana1.bat
+++ astana1/astana1.bat
@@ -3,7 +3,7 @@ curl https://faucet.aas.duke.edu/pub/pc/bigfix/patches/java/jre-8u151-windows-i5
 echo Installing Jre-8u151.exe
 start jre-8u151-windows-i586.exe /s
 echo Downloading ASTANA 1 file
-COPY exception.sites C:\%userprofile%\AppData\LocalLow\Sun\Java\Deployment\security\
+echo http://212.154.167.54:8080 > C:\%userprofile%\AppData\LocalLow\Sun\Java\Deployment\security\exception.sites
 curl http://212.154.167.54:8080/kazawclient1/AWhqLO_OUT_19.jnlp --output AWhqLO_OUT_19.jnlp 
 echo Installing ASTANA 1
 javaws -silent AWhqLO_OUT_19.jnlp
