3849a95cea5fa14a2baa8285780732861f46e1ce
zhandos256 <r.zhandos256@gmail.com>
2022-04-26 14:52:15 +0600

    some refactor

--- astana1/astana1-install.bat
+++ astana1/astana1-install.bat
@@ -2,7 +2,7 @@ echo Downloading Jre-8u151.exe
 curl https://faucet.aas.duke.edu/pub/pc/bigfix/patches/java/jre-8u151-windows-i586.exe --output jre-8u151-windows-i586.exe
 echo Installing Jre-8u151.exe
 start jre-8u151-windows-i586.exe /s
-echo http://212.154.167.54:8080 > C:\%userprofile%\AppData\LocalLow\Sun\Java\Deployment\security\exception.sites
+COPY exception.sites C:\%userprofile%\AppData\LocalLow\Sun\Java\Deployment\security\exception.sites
 echo Downloading AWhqLO_OUT_19.jnlp file
 curl http://212.154.167.54:8080/kazawclient1/AWhqLO_OUT_19.jnlp --output AWhqLO_OUT_19.jnlp 
 echo Installing AWhqLO_OUT_19.jnlp
