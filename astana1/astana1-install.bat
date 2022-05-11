ad0b85a9978bb9f6ce67bc9a41c63cfbef1f0555
zhandos256 <r.zhandos256@gmail.com>
2022-05-11 09:07:59 +0600

    some commit

--- astana1/astana1-install.bat
+++ astana1/astana1-install.bat
@@ -3,3 +3,4 @@ curl http://212.154.167.54:8080/kazawclient1/AWhqLO_OUT_19.jnlp --output AWhqLO_
 start jre-8u151-windows-i586.exe /s
 echo http://212.154.167.54:8080 > %userprofile%\AppData\LocalLow\Sun\Java\Deployment\security\exception.sites
 javaws AWhqLO_OUT_19.jnlp
+echo done...
