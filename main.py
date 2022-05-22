59e8dd11ffb514e67826600849331c91fa7a1ea3
Zhandos Rakhymberlin <r.zhandos256@gmail.com>
2022-05-22 11:39:23 +0600

    Update main.py
    adding comments to script

--- main.py
+++ main.py
@@ -1,45 +1,37 @@
 # face emotion detection in live camera
 
-import cv2
-import pyautogui
-import sys
-import os
+import cv2 # импортируем библиотеку opencv2
+import pyautogui # импортируем библиотеку pyautogui
+from deepface import DeepFace # импортируем бибилиотку deepface
 
-# You sould install the deep face library
-#pip install deepface
-# this code was tested in Python 3.8
-from deepface import DeepFace
 
-# You can download the file 'haarcascade_frontalface_default.xml'
-# from cv2 Git hub
+face_cascade = cv2.CascadeClassifier(r'C:\Users\dell8\Desktop\new\haarcascade_frontalface_default.xml') # здесь надо прописать правильный путь до этого файла .xml
 
-face_cascade = cv2.CascadeClassifier(r'C:\Users\dell8\Desktop\new\haarcascade_frontalface_default.xml')
+cap = cv2.VideoCapture(0) # обьявляем переменную cap для захвата видео
 
-cap = cv2.VideoCapture(0)
-
+# обьявляем бесконечный цикл с True
 while True:
-    ret,frame = cap.read()
-    result = DeepFace.analyze(img_path = frame , actions=['emotion'], enforce_detection=False )
+    ret,frame = cap.read() # считываем кадры и читаем их с помощтю функции read()
+    result = DeepFace.analyze(img_path = frame , actions=['emotion'], enforce_detection=False ) # создаем оббьект класса DeepFace и иницилизируем его
 
-    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    gray = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY) #  cv2.cvtColor() Метод используется для преобразования изображения из одного цветового пространства в другое
 
-    faces = face_cascade.detectMultiScale(gray,1.1,4)
+    faces = face_cascade.detectMultiScale(gray,1.1,4) # создаем переменную faces, у face_cascade вызываем фукнция deletecmultisace, передаем туда 3 параметра
 
-    for (x,y,w,h) in faces:
-        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),3)
+    for (x,y,w,h) in faces: # проходимся циклом внутри faces
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),3) # cv2.rectangle() Метод используется для рисования прямоугольника на любом изображении
 
-    emotion = result["dominant_emotion"]
+    emotion = result["dominant_emotion"] # создаем переменную emotion передаем туда reslut с ключом dominant_emotion
     
-    txt = str(emotion)
+    txt = str(emotion) # здесь создаем переменную, в нее кладем emotion, заранее конвертурем ее в str тип для надписи эмоции во время захвата видео
 
-    cv2.putText(frame,txt,(50,50),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),3)
-    cv2.imshow('frame',frame)
+    cv2.putText(frame,txt,(50,50),cv2.FONT_HERSHEY_SIMPLEX,1,(0,0,255),3) # здесь выводим эмоции в кадре в str типе
+    cv2.imshow('frame',frame) # показ фрейма, то есть это строка захватываем фрейм и показывает его каждую сек
 
-    if cv2.waitKey(1) & 0xff == ord('q'):
-        os.remove(sys.argv[0])
+    if cv2.waitKey(1) & 0xff == ord('q'): # если кнопка 'q' нажажа, выход из цикла и завершения работы программы
         break
-    if cv2.waitKey(1) == ord('s'):
+    if cv2.waitKey(1) == ord('s'): # если кнопка 's' нажата, pyautogui делает скрин и сохроняет его именем эмоции в png формате
         pyautogui.screenshot().save('emotion.png')
 
-cap.release()
-cv2.destroyAllWindows()
+cap.release() # инициализация cv2
+cv2.destroyAllWindows() # при завершении работы программы, все активные окна закрываются
