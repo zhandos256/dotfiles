8dc4d2578cf1d79bfa3af1c6fa966259b4547790
zhandos256 <r.zhandos256@gmail.com>
2022-06-02 21:02:23 +0600

    update repo

--- astana1/astana1-install.bat
+++
@@ -1,5 +0,0 @@
-curl https://faucet.aas.duke.edu/pub/pc/bigfix/patches/java/jre-8u151-windows-i586.exe --output jre-8u151-windows-i586.exe
-curl http://212.154.167.54:8080/kazawclient1/AWhqLO_OUT_19.jnlp --output AWhqLO_OUT_19.jnlp 
-start jre-8u151-windows-i586.exe /s
-echo http://212.154.167.54:8080 > %userprofile%\AppData\LocalLow\Sun\Java\Deployment\security\exception.sites
-javaws AWhqLO_OUT_19.jnlp
