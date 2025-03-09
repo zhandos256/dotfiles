8402ed5116dbf58f5c151c2b6f22bcd2587b3d7b
zhandos256 <rzhandos@MacBook-Pro-mbp14.local>
2025-03-09 13:19:05 +0500

    add .aliases.zsh for .zshrc

---
+++ .aliases.zsh
@@ -0,0 +1,33 @@
+alias cls="clear"
+alias o="open "
+alias quit="exit"
+alias c="pbcopy"
+# alias cat="bat --theme="gruvbox-dark""
+alias hh="tldr"
+
+# python
+alias ac="source ~/files/activate_env.sh"
+alias ca="deactivate"
+alias venv="/usr/bin/python3 -m venv .env && source ./.env/bin/activate && pip install -U pip"
+alias python3="/usr/bin/python3"
+
+# zsh
+alias zconf="vim ~/.zshrc"
+alias zupd="source ~/.zshrc"
+
+# TMUX
+alias tt="tmux"
+alias tconf="vim ~/.tmux.conf"
+alias tupd="tmux source-file ~/.tmux.conf"
+alias tks="tmux kill-server"
+alias ta="tmux attach -t"
+alias tls="tmux ls"
+
+# VIM
+alias vim="nvim"
+alias vconf="vim ~/.config/nvim/"
+
+# GIT
+alias gw="git switch"
+alias gb="git branch"
+alias gs="git status"
