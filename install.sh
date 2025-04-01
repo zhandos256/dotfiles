699ba23138329125d5641924e5eb198f25baa280
zhandos256 <r.zhandos256@gmail.com>
2025-04-01 11:18:05 +0500

    install.sh comments

--- install.sh
+++ install.sh
@@ -1,16 +1,35 @@
 #!/bin/bash
 
+# Установка Tmux Plugin Manager (TPM)
+# Клонирование репозитория TPM в директорию плагинов tmux
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# Установка Packer - менеджера плагинов для Neovim
+# Клонирование репозитория Packer в директорию плагинов Neovim
 git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
+# Создание необходимых директорий для конфигураций
+# Создание директорий для Neovim и skhd если они не существуют
 mkdir -p $HOME/.config/nvim $HOME/.config/skhd
 
+# Копирование конфигурационных файлов
+# Копирование конфигурации Neovim
 cp -R ./nvim $HOME/.config/
+
+# Копирование конфигурации Tmux
 cp ./.tmux.conf $HOME/
+
+# Копирование конфигурации Vim
 cp ./.vimrc $HOME/
-cp ./.vimrc $HOME/
+
+# Копирование конфигурации Zsh
 cp ./.zshrc $HOME/
+
+# Копирование алиасов Zsh
 cp ./.aliases.zsh $HOME/
+
+# Копирование конфигурации skhd (горячие клавиши)
 cp ./skhdrc $HOME/.config/skhd/
 
+# Завершение установки
 echo Done
