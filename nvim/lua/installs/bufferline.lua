11ee571c1286acedafe8ebda8899ad71f0d320a1
zhandos256 <r.zhandos256@gmail.com>
2025-04-06 08:04:01 +0500

    nothing

--- nvim/lua/installs/bufferline.lua
+++ nvim/lua/installs/bufferline.lua
@@ -2,7 +2,7 @@ vim.opt.termguicolors = true
 require("bufferline").setup({
 	options = {
 		mode = "tabs",
-		separator_style = "thin",
+		-- separator_style = "thin",
 		always_show_bufferline = false,
 		show_buffer_close_icons = false,
 		show_close_icon = false,
