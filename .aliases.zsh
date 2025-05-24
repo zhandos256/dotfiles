a87528be741516196b4686933490893386577c1a
Zhandos Rakhymberlin <r.zhandos256@gmail.com>
2025-05-24 17:01:48 +0500

    Update .aliases.zsh

--- .aliases.zsh
+++ .aliases.zsh
@@ -7,11 +7,11 @@ alias hh="tldr"
 # python
 alias ac="source ~/files/activate_env.sh"
 alias ca="deactivate"
-alias venv="/usr/bin/python3 -m venv .env && source ./.env/bin/activate && pip install -U pip"
+alias venv="/usr/bin/python3 -m venv .venv && source ./.venv/bin/activate && pip install -U pip"
 alias python3="/usr/bin/python3"
 
 # zsh
-alias zconf="nvim ~/.zshrc"
+alias zconf="vim ~/.zshrc"
 alias zupd="source ~/.zshrc"
 
 # TMUX
@@ -21,10 +21,6 @@ alias tupd="tmux source-file ~/.tmux.conf"
 alias tks="tmux kill-server"
 alias ta="tmux attach -t"
 alias tls="tmux ls"
-
-# VIM
-alias v="nvim"
-alias vconf="nvim ~/.config/nvim/"
 
 # GIT
 alias gw="git switch"
